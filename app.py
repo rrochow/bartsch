119efe03be396e24823b30e8e35be0b52cd3f246
rodmast <rodmast@gmail.com>
2014-12-29 19:09:43 -0300

    implementacion del servidor de correo y registro de usuario, avance en el carro de compras

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+#libreria validacion de mail solamente chrome
+import re
+#libreria de servidor de correo
+from flask.ext.mail import Message, Mail
 from sqlite3 import dbapi2 as sqlite3
 from flask import (
     Flask,
@@ -7,19 +11,52 @@ from flask import (
     request,
     flash,
     redirect,
-    session
+    session,
 )
+#inicializacion del email
+DEBUG = True
+SECRET_KEY = 'hidden'
+USERNAME = 'secret'
+PASSWORD = 'secret'
+MAIL_SERVER='smtp.gmail.com'
+MAIL_PORT=465
+MAIL_USE_TLS = False
+MAIL_USE_SSL= True
+MAIL_USERNAME = 'testcerveza@gmail.com'
+MAIL_PASSWORD = 'grupotaller'
+
+#inicializacion carro de compras
+carro = 0
 
 app = Flask(__name__)
 
+app.config.from_object(__name__)
+mail = Mail(app)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
+#conexion a la base de datos
+def connect_db():
+    """Retorna una conexión a la BD"""
+    path_to_db = 'basedatos.db'
+    rv = sqlite3.connect(path_to_db)
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    """Crea las tablas y datos de prueba"""
+    db = connect_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+#Login y Logout
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         user = request.form["user"]
         password = request.form["password"]
-        db = connect_db2()
+        db = connect_db()
         result = db.execute('SELECT correo, clave FROM cliente WHERE correo=? AND clave = ?',[user, password])
         matches = result.fetchall()
         db.close()
@@ -38,28 +75,6 @@ def logout():
     session.pop('username',None)
     return redirect(url_for('show_home'))
 
-def connect_db():
-    """Retorna una conexión a la BD"""
-    path_to_db = 'entry.db'
-    rv = sqlite3.connect(path_to_db)
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def connect_db2():
-    """Retorna una conexión a la BD"""
-    path_to_db = 'basedatos.db'
-    rv = sqlite3.connect(path_to_db)
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    """Crea las tablas y datos de prueba"""
-    db = connect_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
-
 
 @app.route('/')
 def show_home():
@@ -71,37 +86,44 @@ def show_bartsch():
 
 @app.route('/cervezas')
 def show_cervezas():
-    db = connect_db2()
+    db = connect_db()
     cur = db.execute('SELECT nombre, descripcion, grados,img_url FROM cerveza ORDER BY id DESC')
     cerveza = cur.fetchall()
     db.close()
-    return render_template('productos.html', cervezas=cerveza)
+    return render_template('cervezas.html', cervezas=cerveza)
+
+#carro de compras
 
 @app.route('/carro')
-def new_compra():
-    db = connect_db2()
-    cur = db.execute('SELECT p.id,p.nombre, p.descripcion,p.precio_neto,b.stock,p.img_url FROM producto p,bodega b WHERE  p.id=b.id_producto ORDER BY p.id DESC')
+def show_productos():
+    db = connect_db()
+    cur = db.execute('SELECT id,nombre, descripcion,precio_neto,img_url FROM producto ORDER BY id DESC')
     producto = cur.fetchall()
     db.close()
 
-    db = connect_db2()
-    cur = db.execute('SELECT nombre, descripcion,grados,img_url FROM cerveza ORDER BY id DESC')
-    cerveza = cur.fetchall()
+    return render_template('carro_productos.html', productos=producto,cantidad = carro)
+
+@app.route('/carro/<int:id_producto>/<int:cantidad>')
+def agregar_al_carro(id_producto,cantidad):
+    global carro
+    carro = cantidad + carro
+    db = connect_db()
+    cur = db.execute('SELECT id,nombre, descripcion,precio_neto,img_url FROM producto ORDER BY id DESC')
+    producto = cur.fetchall()
     db.close()
 
-    return render_template('carro.html', productos=producto, cervezas=cerveza)
+    return render_template('carro_productos.html', productos=producto,cantidad=carro)
 
-@app.route('/carro', methods=['GET', 'POST'])
+@app.route('/carro_compras')
 def show_carro():
-    if request.method == 'GET':
-        return "Ajilao"
-    elif request.method == 'POST':
-    	if request.form['boton_carro'] == '5':
-    		return render_template('index.php')
-    	else:
-    		return request.form['cantidad']
-    else:
-        return "Acceso denegado"
+    db = connect_db()
+    cur = db.execute('SELECT p.id,p.nombre, p.descripcion,p.precio_neto,b.stock,p.img_url FROM producto p,bodega b WHERE  p.id=b.id_producto ORDER BY p.id DESC')
+    producto = cur.fetchall()
+    db.close()
+    return render_template('carro.html',productos=producto)
+
+
+#Perfil del Cliente
 
 @app.route('/perfil')
 def show_perfil():
@@ -115,51 +137,58 @@ def show_empresa():
 def show_cuenta():
     return render_template('perfil_cuenta.html')
 
+#registro de usuario
 
-@app.route('/form', methods=['GET', 'POST'])
-def new_post():
-    if request.method == 'GET':
-        return render_template('form.html')
-    elif request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-        db = connect_db()
-        db.execute(
-            'INSERT INTO entry (title, description) values (?, ?)',[title, description]
-        )
-        db.commit()
-        db.close()
-        return u"Operación exitosa"
+def is_email_address_valid(correo):
+    """Validate the email address using a regex."""
+    if not re.match("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$", correo):
+        return False
     else:
-        return "Acceso denegado"
+        return True
 
-@app.route('/registro', methods=['GET', 'POST'])
+@app.route('/registro', methods=['GET','POST'])
 def new_registro():
     if request.method == 'GET':
-        return render_template('registrarse.html')
+        return render_template("registrarse.html")
     elif request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
+        correo = request.form['correo']
         db = connect_db()
-        db.execute(
-            'INSERT INTO entry (title, description) values (?, ?)',[title, description])
-        db.commit()
+        result = db.execute('SELECT correo FROM cliente WHERE correo=?', [correo])
+        matches = result.fetchall()
         db.close()
-        return u"Operación exitosa"
+
+        if len(matches) > 0: #The user and pass combination exits
+            return render_template('registrarse.html',error='correo ya registrado')
+        else:
+            correo = request.form['correo']
+            clave = request.form['clave']
+            db = connect_db()
+            db.execute(
+                'INSERT INTO cliente (correo, clave) values (?, ?)',[correo, clave]
+            )
+            db.commit()
+            db.close()            
+            return render_template('registrarse.html',error='Registrado con exito')  
     else:
-        return "Acceso denegado"
+        return render_template("home.html")
+
+
+#Metodos Email
 
 @app.route('/contacto', methods=['GET', 'POST'])
 def new_contacto():
     if request.method == 'GET':
         return render_template('contacto.html')
     elif request.method == 'POST':
-    	nombre = request.form['nombre']
-    	email = request.form['email']
-    	mensaje = request.form['mensaje']
-    	return mensaje
-    else:
-        return "Acceso denegado"
+        mensaje = Message(
+            'Contacto-Cerveza Bartsch',
+            sender=request.form['nombre'],
+            recipients=['testcerveza@gmail.com']
+        )
+        mensaje.body = "De : "+request.form['nombre']+"\n\n"+request.form['mensaje'] + "\n\nCorreo : " +request.form['email']
+        mail.send(mensaje)
+        return "Send"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
